ba04d056caf064a90d43a80edff209b00c9333c8
phamthangks <phamthang6636@gmail.com>
2025-10-07 17:15:23 +0700

    Bai6 - Dynamic Text, Properties, Attributes

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderLayoutComponent } from './shared/header-layout/header-layout.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderLayoutComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'first-project';
+  //Text
+  title = {
+    name: 'first-project',
+    old: 2024,
+  };
+
+  //Properties
+  isDisabled = false;
+
+  //Atributes
+  contentImage = 'Hi Thang'
 }
---
+++ first-project/src/app/shared/header-layout/header-layout.component.ts
@@ -0,0 +1,11 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'header-layout',
+  templateUrl: './header-layout.component.html',
+  styleUrl: './header-layout.component.css'
+})
+
+export class HeaderLayoutComponent {
+
+}
