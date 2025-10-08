68cb541757fc1c30ca9eedc26b42a5abd1a64b67
phamthangks <phamthang6636@gmail.com>
2025-10-08 18:56:50 +0700

    Bai8 - Two-way Binding

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderLayoutComponent } from './shared/header-layout/header-layout.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderLayoutComponent],
+  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -12,6 +13,8 @@ export class AppComponent {
   nameBtn = 'Click Me!';
 
   clickMessage = '';
+
+  bindingMessage = '';
 
   handleClickMe():void {
     this.clickMessage = 'Button is clicked!';
