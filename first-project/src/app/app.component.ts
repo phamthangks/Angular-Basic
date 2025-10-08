b5c00afb7a50da991258dc0e2cf2de85e56b1ae1
phamthangks <phamthang6636@gmail.com>
2025-10-08 20:03:40 +0700

    Bai10 - Pipes

--- first-project/src/app/app.component.ts
+++ first-project/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderLayoutComponent } from './shared/header-layout/header-layout.component';
 import { FormsModule } from '@angular/forms';
+import { CurrencyPipe } from './shared/pipes/CurrentcyPipe.pipe';
+import { UpperCasePipe } from './shared/pipes/UpperCasePipe.pipe';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule],
+  imports: [RouterOutlet, HeaderLayoutComponent, FormsModule, CurrencyPipe, UpperCasePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
