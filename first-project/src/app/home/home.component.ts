6c5bc335aec817b42f9a92727d22d100417d9dd6
phamthangks <phamthang6636@gmail.com>
2025-10-15 20:46:06 +0700

    Bai20 - Creation (constructor, ngOnInit)

--- first-project/src/app/home/home.component.ts
+++ first-project/src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { ProductItems } from '../shared/types/productItem';
 import { ProductItemComponent } from '../shared/product-item/productItem.component';
@@ -11,7 +11,7 @@ import { ProductItemComponent } from '../shared/product-item/productItem.compone
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
   nameBtn = 'Click Me!';
 
   clickMessage = '';
@@ -25,6 +25,18 @@ export class HomeComponent {
     {id: 4, name: 'mlb f3', price: 700000, image: 'assets/images/shoe1.png'},
   ];
 
+   ngOnInit(): void {
+    console.log('Initalize Component 1');
+    fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())
+      .then(json => console.log(json))
+  }
+
+  constructor() {
+    console.log('Initalize Component 2');
+  }
+
+ 
 
   handleClickMe():void {
     this.clickMessage = 'Button is clicked!';
